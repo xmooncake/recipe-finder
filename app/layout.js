9a557aaee64bf48fcc4a5da9069ad2a5cb35f31e
xmooncake <s.gierlachpl@gmail.com>
2024-06-05 20:29:58 +0200

    chore: Remove unused CSS file from layout.js

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,6 @@ export default function RootLayout({ children }) {
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true}/>
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
-      <link href="./globals.css" rel="stylesheet"/>
       </head>
       <body className={montserrat.className}>{children}</body>
     </html>
